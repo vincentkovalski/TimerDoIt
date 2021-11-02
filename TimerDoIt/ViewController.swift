4c1394922dc67e433b24f5a83eb93144aea61d12
Виктор Ковалевский <kovall89@gmail.com>
2021-11-02 09:40:12 +0300

    Initial Commit

---
+++ TimerDoIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerDoIt
+//
+//  Created by Ковалевский on 02.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
